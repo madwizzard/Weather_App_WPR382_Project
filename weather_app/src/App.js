415d490a6d19a0a45b709a6a912b4b2f658f9605
EstiaanBritz <82169901+madwizzard@users.noreply.github.com>
2023-05-31 14:55:37 +0200

    making the app

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import hot_bg from './assets/Hot.jpg'
+import cold_bg from './assets/Cold.jpg'
+import Descriptions from './components/Descriptions';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url{${cold_bg}}` }}>
+
+
+      <div className="overlay">
+
+        <div className="container">
+
+          <div className="section section__input">
+
+            <input type="text" className="Zip" placeholder="Enter Your zip code..." />
+            <button>°C</button>
+            <button>°F</button>
+
+          </div>
+
+          <div className="section section__temp">
+
+            <div className="icon">
+
+              <h3>Brakpan, ZA</h3>
+              <img src="" alt="weather icon that displays the current weather" />
+              <h3>Light Rain</h3>
+
+            </div>
+            <div className="temp">
+              <h1>14°C</h1>
+            </div>
+
+          </div>
+          {/*bottom discription*/}
+          <Descriptions />
+        </div>
+
+      </div>
+
+
     </div>
+
   );
 }
 
---
+++ weather_app/src/components/Descriptions.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { AiOutlineArrowDown } from 'react-icons/ai'
+import "./descriptions.css";
+const Descriptions = () => {
+    return (
+        <div className="section section_descript">
+            <div className="card">
+                <div className="descript__card-icon">
+                    <AiOutlineArrowDown />
+                    <small>min</small>
+                </div>
+                <h2>14 °C</h2>
+            </div>
+            <div className="card">
+                <div className="descript__card-icon">
+                    <AiOutlineArrowDown />
+                    <small>min</small>
+                </div>
+                <h2>14 °C</h2>
+            </div>
+            <div className="card">
+                <div className="descript__card-icon">
+                    <AiOutlineArrowDown />
+                    <small>min</small>
+                </div>
+                <h2>14 °C</h2>
+            </div>
+            <div className="card">
+                <div className="descript__card-icon">
+                    <AiOutlineArrowDown />
+                    <small>min</small>
+                </div>
+                <h2>14 °C</h2>
+            </div>
+            <div className="card">
+                <div className="descript__card-icon">
+                    <AiOutlineArrowDown />
+                    <small>min</small>
+                </div>
+                <h2>14 °C</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Descriptions
