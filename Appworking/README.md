1cf7cdeae6bee540cc604abbebd8a7573c61b687
EstiaanBritz <82169901+madwizzard@users.noreply.github.com>
2023-06-07 17:32:46 +0200

    Adding descriptions

--- Appworking/README.md
+++ Appworking/README.md
@@ -1,2 +1,29 @@
 # Weather_App_WPR382_Project
- Project that uses react to create a weather app
+ This is my first project where I used React.
+ I learned a lot about React, express and API's and how to use them. 
+
+
+
+# React Weather App
+
+React is a JavaScript library for building user interfaces. 
+I tried to make the app as modular and simple as possible.
+
+## Running the app
+Open a terminal within the src folder and run the following commands:
+
+```bash
+node server.js
+```
+Open a second terminal in the weather-app folder and run the following commands:
+```bash
+npm start
+```
+## Usage
+
+To use the app, simply type in the zip code of the city **in South Africa** you want to get the weather for and press enter.
+You can click on the °C or °F to change the temperature from Celsius to Fahrenheit and vice versa.
+
+
+
+
--- Appworking/weather-app/src/server.js
+++ Appworking/weather-app/src/server.js
@@ -8,6 +8,8 @@ const cors = require('cors');
 app.use(cors());
 const API_KEY = "8bd57e7c8b4c47a7d74835a2f88ae461";
 
+
+// This is a server that will fetch weather data from the OpenWeatherMap API
 app.get("/weather", async (req, res) => {
   const { zip, units } = req.query;
 
@@ -16,6 +18,7 @@ app.get("/weather", async (req, res) => {
       `https://api.openweathermap.org/data/2.5/weather?q=${zip},za&appid=${API_KEY}&units=${units}`
     );
 
+    // This is the data that we want to send back to the client. It is named weatherData
     const weatherData = {
       description: response.data.weather[0].description,
       iconURL: `http://openweathermap.org/img/w/${response.data.weather[0].icon}.png`,
@@ -29,7 +32,7 @@ app.get("/weather", async (req, res) => {
       country: response.data.sys.country,
       name: response.data.name,
     };
-
+    // This is the response that we send back to the client that incudes the weatherData
     res.json(weatherData);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch weather data" });
